3d6a3658e3caec76650aebf2692ac8fd0478a9d9
IndiraKeerthana <p.indirakeerthana@gmail.com>
2026-05-25 20:19:39 +0530

    Final minute changes

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <div className="container p-5 mb-5 px-3">
             <div className="row text-center">
-                <img src="/media/homeHero.png" alt="Hero Image"  className = "mb-5" style = {{width: "70%", margin: "0 auto"}}></img>
+                <img src="/media/homeHero.png" alt="Hero Image"  className = "mb-2" style = {{width: "65%", margin: "0 auto"}}></img>
                 <h1 className="mt-5 fs-3 mb-2" style = {{color: "#424242", fontSize: "1.75rem"}}>Invest in everything</h1>
                 <p className="text-muted fs-5 mb-3 mt-2" style = {{fontWeight: "400"}}>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more</p>
                 <button className = "p-2 btn btn-primary fs-5 mb-5 mt-4" style= {{width: "18%",height: "3.15rem", margin: "0 auto"}}>Sign up for free</button>
