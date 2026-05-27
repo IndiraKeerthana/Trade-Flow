806eb935b000f9904e0596aca6ab494d3baf8c53
IndiraKeerthana <p.indirakeerthana@gmail.com>
2026-05-28 00:11:43 +0530

    Enhanced Navbar component with improved mobile toggle button using Font Awesome icons and added a placeholder button for desktop. Updated CSS to remove focus shadow on toggler for a cleaner look.

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -9,11 +9,21 @@ function Navbar() {
                 <Link className="navbar-brand ms-2 ms-md-4" to="/">
                     <img src="media/TradeFlow_logo.svg" className="img-fluid" style={{ width: '40%', minWidth: "120px", maxWidth: "180px" }} alt="Logo" />
                 </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                {/* Mobile: Font Awesome bars acts as Bootstrap menu toggle */}
+                <button
+                    className="navbar-toggler border-0 shadow-none d-lg-none"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <i className="fa-solid fa-bars fa-lg" style={{ color: "#424242" }}></i>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mb-lg-0 ms-auto">
+                    <ul className="navbar-nav mb-lg-0 ms-auto align-items-lg-center">
                         <li className="nav-item">
                             <Link className="nav-link" aria-current="page" to="/signup">Signup</Link>
                         </li>
@@ -34,10 +44,16 @@ function Navbar() {
                             <Link className="nav-link" to="/support">Support</Link>
                         </li>
 
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
+                        {/* Desktop placeholder menu button for future features */}
+                        <li className="nav-item d-none d-lg-block">
+                            <button
+                                type="button"
+                                className="nav-link border-0 bg-transparent text-decoration-none p-0 ms-2"
+                                aria-label="Open menu"
+                                title="Menu"
+                            >
                                 <i className="fa-solid fa-bars"></i>
-                            </a>
+                            </button>
                         </li>
                     </ul>
                 </div>
