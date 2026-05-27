db4d85c023c5f98f65b378ed6ebf3446cd6f93c3
IndiraKeerthana <p.indirakeerthana@gmail.com>
2026-05-27 23:30:15 +0530

    Added the left section with media

--- src/landing_page/products/LeftSection.js
+++ src/landing_page/products/LeftSection.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
-function LeftSection() {
-    return(
-        <h1>Left Section</h1>
+function LeftSection({
+    imageURL,
+    productName,
+    productDescription,
+    tryDemo,
+    learnMore,
+    googlePlay,
+    appStore
+}) {
+    return (
+        <div className="container-fluid px-3 px-md-5 my-5">
+            <div className="row align-items-center">
+                {/* Left Image */}
+                <div className="col-12 col-md-7 text-center mb-5 mb-md-0">
+                    <img src={imageURL} className="img-fluid" style={{ maxWidth: "90%" }} />
+                </div>
 
+                {/* Right Text */}
+                <div className="col-12 col-md-4 text-center text-md-start" style={{ color: "#424242" }}>
+
+                    <h2 className="fs-4 mb-4" style={{ color: "#424242" }}>{productName}</h2>
+
+                    <p style={{
+                        lineHeight: "1.8",
+                        fontSize: "1rem"
+                    }}>{productDescription}</p>
+
+                    <div className="mt-4 d-flex gap-4 flex-wrap justify-content-center justify-content-md-start">
+                        <a href={tryDemo} style={{ textDecoration: "none" }}>Try Demo <i class="fa-solid fa-arrow-right-long"></i></a>
+
+                        <a href={learnMore} style={{ marginLeft: "50px", textDecoration: "none" }} >Learn More <i class="fa-solid fa-arrow-right-long"></i></a>
+                    </div>
+
+                    <div className="mt-4 d-flex gap-3 flex-wrap justify-content-center justify-content-md-start">
+                        <a href={googlePlay}>
+                            <img src="media\googlePlayBadge.svg" className="img-fluid"
+                                style={{ maxWidth: "170px" }}
+                            />
+                        </a>
+
+                        <a href={appStore}>
+                            <img src="media\appstoreBadge.svg" className="img-fluid"
+                                style={{ maxWidth: "170px" }} />
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
