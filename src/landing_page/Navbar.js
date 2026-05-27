d0a34e9ab55f7dea37a6ef66e587131a067e523f
IndiraKeerthana <p.indirakeerthana@gmail.com>
2026-05-28 00:01:09 +0530

    Refactored layout and styling for landing page components, including OpenAccount, Footer, Navbar, and NotFound. Improved responsiveness and spacing, and updated CSS for better mobile support.

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -4,39 +4,39 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
 
-        <nav class="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: '#FFF' }}>
-            <div class="container">
-                <Link class="navbar-brand ml-4" to="/">
-                    <img src="media/TradeFlow_logo.svg" style={{ width: '40%' }} alt="Logo" />
+        <nav className="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: '#FFF' }}>
+            <div className="container">
+                <Link className="navbar-brand ms-2 ms-md-4" to="/">
+                    <img src="media/TradeFlow_logo.svg" className="img-fluid" style={{ width: '40%', minWidth: "120px", maxWidth: "180px" }} alt="Logo" />
                 </Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mb-lg-0 ms-auto">
-                        <li class="nav-item">
-                            <Link class="nav-link" aria-current="page" to="/signup">Signup</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mb-lg-0 ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/signup">Signup</Link>
                         </li>
 
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/about">About</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
 
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/product">Product</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/product">Product</Link>
                         </li>
 
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/pricing">Pricing</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pricing">Pricing</Link>
                         </li>
 
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/support">Support</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/support">Support</Link>
                         </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <i class="fa-solid fa-bars"></i>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="fa-solid fa-bars"></i>
                             </a>
                         </li>
                     </ul>
--- src/landing_page/NotFound.js
+++ src/landing_page/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NotFound() {
     return (
-        <div className="container p-5 mt-5 mb-5 px-3">
+        <div className="container py-4 py-lg-5 mt-5 mb-5 px-3">
             <div className="row text-center">
                 <h1 className="mt-5 fs-3 mb-2" style = {{color: "#424242", fontSize: "1.75rem"}}>404 Not Found</h1>
                 <p className="text-muted fs-5 mb-3 mt-2" style = {{fontWeight: "400"}}>The page your looking for does not exist.</p>
